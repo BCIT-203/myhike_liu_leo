28a653ea694e0f427553e9dab07f0aa7533248db
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-10-29 11:21:47 -0700

    add hikes data

--- scripts/script.js
+++ scripts/script.js
@@ -59,3 +59,63 @@ function readQuote(day) {
         });
 }
 readQuote("tuesday");        //calling the function
+
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeLoop(max) {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+    for (i = 1; i <= max; i++) {
+        hikesRef.add({ //add to database, autogen ID
+            code: Math.ceil(Math.random()*3)%3?Math.ceil(Math.random()*3)%2?"AM01":"BBY01":"NV01",
+            name: "Trail " + i,
+            city: "North Vancouver",
+            province: "BC",
+            details: "Elmo says this hike is amazing! You will love going on hike " + i,
+            level: Math.ceil(Math.random()*3)%3?Math.ceil(Math.random()*3)%2?"easy":"moderate":"hard",
+            lat: 49+Math.random(),    //randomly different
+            lng: -122+Math.random(),  //randomly different
+            length: Math.ceil(100*Math.random())/10,        //number value
+            hike_time: Math.ceil(180*Math.random()),     //number value
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        })
+   }
+}
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable.
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allHikes=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var details = doc.data().details;  // get value of the "details" key
+                var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var hikeLength = doc.data().length; //gets the length field
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("hikes");  //input param is the name of the collection
