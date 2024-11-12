d60674a7c087cebfe6e753fc6ac9d782501473a9
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-12 10:57:17 -0800

    implement bookmark

--- scripts/main.js
+++ scripts/main.js
@@ -67,6 +67,9 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
                 newcard.querySelector('a').href = "eachHike.html?docID="+docID;
 
+                newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
+                newcard.querySelector('i').onclick = () => saveBookmark(docID);
+
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
@@ -76,6 +79,16 @@ function displayCardsDynamically(collection) {
                 document.getElementById(collection + "-go-here").appendChild(newcard);
 
                 //i++;   //Optional: iterate variable to serve as unique ID
+
+
+                currentUser.get().then(userDoc => {
+                    //get the user name
+                    var bookmarks = userDoc.data().bookmarks;
+                    if (bookmarks.includes(docID)) {
+                        document.getElementById('save-' + docID).innerText = 'bookmark';
+                    }
+                })
+
             })
         })
 }
@@ -136,3 +149,26 @@ function doAll() {
     });
 }
 doAll();
+
+
+//-----------------------------------------------------------------------------
+// This function is called whenever the user clicks on the "bookmark" icon.
+// It adds the hike to the "bookmarks" array
+// Then it will change the bookmark icon from the hollow to the solid version.
+//-----------------------------------------------------------------------------
+function saveBookmark(hikeDocID) {
+		// Manage the backend process to store the hikeDocID in the database, recording which hike was bookmarked by the user.
+    currentUser.update({
+						// Use 'arrayUnion' to add the new bookmark ID to the 'bookmarks' array.
+		        // This method ensures that the ID is added only if it's not already present, preventing duplicates.
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
+        })
+				// Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
+        .then(function () {
+            console.log("bookmark has been saved for" + hikeDocID);
+            let iconID = 'save-' + hikeDocID;
+            //console.log(iconID);
+						//this is to change the icon of the hike that was saved to "filled"
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
+}
